ffb7e2bef2a6f3a66b94047841fb2cb1b455e024
carles <carles.ruis@gmail.com>
2019-04-12 15:31:25 +0200

    update koin dependency

--- app/src/main/kotlin/com/carles/carleskotlin/Modules.kt
+++ app/src/main/kotlin/com/carles/carleskotlin/Modules.kt
@@ -12,18 +12,21 @@ import com.carles.carleskotlin.poi.ui.PoiListView
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import org.koin.android.ext.koin.androidContext
-import org.koin.dsl.module.module
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 private const val BASE_URL = "https://t21services.herokuapp.com"
+private val uiScheduler = named("uiScheduler")
+private val processScheduler = named("processScheduler")
 private fun providePoiService(retrofit: Retrofit) = retrofit.create(PoiService::class.java)
 
 val appModule = module {
     single { PreferenceManager.getDefaultSharedPreferences(androidContext()) }
-    single("uiScheduler") { AndroidSchedulers.mainThread() }
-    single("processScheduler") { Schedulers.io() }
+    single(uiScheduler) { AndroidSchedulers.mainThread() }
+    single(processScheduler) { Schedulers.io() }
 
     single {
         Retrofit.Builder()
@@ -39,8 +42,8 @@ val poiModule = module {
     single { PoiLocalDatasource(get()) }
     single { PoiCloudDatasource(get(), get()) }
     single { PoiRepository(get(), get()) }
-    factory { (view: PoiListView) -> PoiListPresenter(view, get("uiScheduler"), get("processScheduler"), get()) }
-    factory { (view: PoiDetailView, id: String) -> PoiDetailPresenter(view, id, get("uiScheduler"), get("processScheduler"), get()) }
+    factory { (view: PoiListView) -> PoiListPresenter(view, get(uiScheduler), get(processScheduler), get()) }
+    factory { (view: PoiDetailView, id: String) -> PoiDetailPresenter(view, id, get(uiScheduler), get(processScheduler), get()) }
 }
 
 val modules = listOf(appModule, poiModule)
