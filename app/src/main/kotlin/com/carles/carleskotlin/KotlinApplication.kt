11cf0e13bc1ee925d699a3a27bcc1dcbd61be0e9
carles <carles.ruis@gmail.com>
2019-04-24 15:00:14 +0200

    update koin initialization

--- app/src/main/kotlin/com/carles/carleskotlin/KotlinApplication.kt
+++ app/src/main/kotlin/com/carles/carleskotlin/KotlinApplication.kt
@@ -2,13 +2,17 @@ package com.carles.carleskotlin
 
 import android.app.Application
 import io.realm.Realm
-import org.koin.android.ext.android.startKoin
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class KotlinApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        startKoin(this, modules)
+        startKoin {
+            androidContext(this@KotlinApplication)
+            modules(modules)
+        }
         Realm.init(this)
     }
 }
